8eea5479f4a946d47292a752c0e6ba5787a4c550
danzinho007 <sedutordanzinho@hotmail.com>
2023-03-03 03:35:12 -0300

    alterei

---
+++ aula01.lua
@@ -0,0 +1,20 @@
+-- String / Texto = Cadeia de caracteres
+
+print("Ola Mundo")
+print("123456")
+print(123456)
+
+print("1+1")
+print(1+1)
+print(1+1-1-1)
+print(2*2)
+print(4/2)
+print(9%2)
+print(2>3)
+print(2<3)
+print(2<=3)
+
+-- Cria uma vari�vel chamada OlaMundo e coloca o texto Ol� Mundo nela
+OlaMundo = "Ola Mundo"
+-- Exibe o valor da vari�vel OlaMundo
+print(OlaMundo)
